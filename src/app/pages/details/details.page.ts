1ed1cbf8413a9219b5a200663d7cbc21e2fe8233
IanRomo <i.romo@duocuc.cl>
2024-10-25 03:20:29 -0300

    modificacion agregar img

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Recipe } from 'src/app/models/recipe.models';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage {
-  constructor(private navCtrl: NavController) {}
+export class DetailsPage implements OnInit {
+  recipeId: string | null = null;
+  recipe: Recipe | undefined;
 
-  // Función para regresar a la página anterior
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit() {
+    // Obtener el ID de la receta desde la URL
+    this.recipeId = this.route.snapshot.paramMap.get('id');
+
+    if (this.recipeId) {
+      // Obtener la receta desde Firebase usando el ID
+      this.recipeService.getRecipeById(this.recipeId).subscribe((data) => {
+        this.recipe = data;
+        console.log('Receta cargada:', this.recipe); // Verificar que los datos se carguen correctamente
+      });
+    }
+  }
+
+  // Método para volver a la página anterior
   goBack() {
-    this.navCtrl.navigateBack('/recipes'); // Vuelve a la página de recetas
+    history.back();
   }
 }
